3a5555a491f30a444529e05656e11db6d616f2bc
itsyuimorii ♥ <109933604+itsyuimorii@users.noreply.github.com>
2025-02-05 20:09:43 -0600

    Update utils.js

--- src/components/utils.js
+++ src/components/utils.js
@@ -8,50 +8,46 @@ const fingerJoints = {
 
 //Drawing function
 export const drawHand = (predictions, ctx) => {
-      console.log("🚀 ~ predictions.forEach ~ keypoints:", predictions);
+  console.log("🚀 ~ predictions.forEach ~ keypoints:", predictions);
 
   if (!predictions || predictions.length === 0) return;
-    predictions.forEach((prediction) => {
-      
-      const keypoints = prediction.keypoints;
-      if (!keypoints || keypoints.length === 0) return;
+  predictions.forEach((prediction) => {
+    const keypoints = prediction.keypoints;
+    if (!keypoints || keypoints.length === 0) return;
 
-      //loop through fingers
+    //loop through fingers
 
-      for (let j = 0; j < Object.keys(fingerJoints).length; j++) {
-        let finger = Object.keys(fingerJoints)[j];
-        for (let k = 0; k < fingerJoints[finger].length - 1; k++) {
-          //Get pairs of joints]
-          const firstJointIndex = fingerJoints[finger][k];
-          const secondJointIndex = fingerJoints[finger][k + 1];
+    for (let j = 0; j < Object.keys(fingerJoints).length; j++) {
+      let finger = Object.keys(fingerJoints)[j];
+      for (let k = 0; k < fingerJoints[finger].length - 1; k++) {
+        //Get pairs of joints]
+        const firstJointIndex = fingerJoints[finger][k];
+        const secondJointIndex = fingerJoints[finger][k + 1];
 
-          //Draw Path
-          ctx.beginPath();
-          ctx.moveTo(
-            keypoints[firstJointIndex].x,
-            keypoints[firstJointIndex].y
-          );
-          ctx.lineTo(
-            keypoints[secondJointIndex].x,
-            keypoints[secondJointIndex].y
-          );
-          ctx.strokeStyle = "plum";
-          ctx.lineWidth = 4;
-          ctx.stroke();
-        }
-      }
-   
-      for (let i = 0; i < keypoints.length; i++) {
-        const x = keypoints[i].x;
-        const y = keypoints[i].y;
-        
-        //start drawing
+        //Draw Path
         ctx.beginPath();
-        ctx.arc(x, y, 3, 0, 2 * Math.PI);
-
-        //set line color
-        ctx.fillStyle = "aqua";
-        ctx.fill();
+        ctx.moveTo(keypoints[firstJointIndex].x, keypoints[firstJointIndex].y);
+        ctx.lineTo(
+          keypoints[secondJointIndex].x,
+          keypoints[secondJointIndex].y
+        );
+        ctx.strokeStyle = "plum";
+        ctx.lineWidth = 4;
+        ctx.stroke();
       }
-    });
+    }
+
+    for (let i = 0; i < keypoints.length; i++) {
+      const x = keypoints[i].x;
+      const y = keypoints[i].y;
+
+      //start drawing
+      ctx.beginPath();
+      ctx.arc(x, y, 3, 0, 2 * Math.PI);
+
+      //set line color
+      ctx.fillStyle = "aqua";
+      ctx.fill();
+    }
+  });
 };
